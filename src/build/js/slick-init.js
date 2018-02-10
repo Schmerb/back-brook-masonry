4bf3c212de3ec8603a0d001f3a8d54fc7ec710a4
Schmerb <mike.schmerbeck@gmail.com>
2018-02-10 13:37:41 -0500

    sped up slick slider on project page

--- src/build/js/slick-init.js
+++ src/build/js/slick-init.js
@@ -15,45 +15,20 @@ const initSlider = () => {
         nextArrow: '<svg class="icon icon-chevron-right"><use xlink:href="#icon-chevron-right"></use></svg>',
         prevArrow: '<svg class="icon icon-chevron-left"><use xlink:href="#icon-chevron-left"></use></svg>',
         infinite: false,
-        speed: 2400,
+        speed: 1200,
         slidesToShow: 1,
         slidesToScroll: 1,
         variableWidth: true,
         responsive: [
-            // {
-            //     breakpoint: 1024,
-            //     settings: {
-            //         slidesToShow: 4,
-            //         slidesToScroll: 4
-            //     }
-            // },
-            // {
-            //     breakpoint: 860,
-            //     settings: {
-            //         slidesToShow: 3,
-            //         slidesToScroll: 3
-            //     }
-            // },
-            // {
-            //     breakpoint: 580,
-            //     settings: {
-            //         slidesToShow: 2,
-            //         slidesToScroll: 2
-            //     }
-            // },
             {
                 breakpoint: 515,
                 settings: {
-                    speed: 2000,
+                    speed: 1200,
                     arrows: false,
                     slidesToShow: 1,
-                    slidesToScroll: 1,
-                    cssEase: 'ease-in-out'
+                    slidesToScroll: 1
                 }
             }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
         ]
     });
     // check if user has seen this in sessionStorage
